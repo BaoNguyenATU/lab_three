9e43d3ff63d40e9f1830dab90e2a0c43e9f4475a
Bao Nguyen <g00420095@atu.ie>
2024-10-20 22:15:09 +0000

    Add year input

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,5 +1,14 @@
 import MovieItem from "./movieitem";
 
+function Movies() {
+    const [year, setYear] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(year);
+    }
+}
+
 const Movies = (props)=>{
     return props.myMovies.map(
         (movie)=>{
@@ -7,5 +16,22 @@ const Movies = (props)=>{
         }
     );
 }
+
+return (
+    <div>
+      <h2>This is my Movies Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
 
 export default Movies;
