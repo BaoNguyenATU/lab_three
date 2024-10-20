a3b4d26b90a7f53eaa1db9ab41c8f4735a105d74
Bao Nguyen <g00420095@atu.ie>
2024-10-20 22:21:04 +0000

    Finishing off the exercise

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,11 +1,15 @@
 import MovieItem from "./movieitem";
 
 function Movies() {
+    {/**State for moviess and posrtr URL */}
     const [year, setYear] = useState('');
-  
+    const [poster, setPoster] = useState('');
+
     const handleSubmit = (e) => {
       e.preventDefault();
+      {/**Logging both years and poster into the console */}
       console.log(year);
+      console.log(poster);
     }
 }
 
@@ -20,17 +24,32 @@ const Movies = (props)=>{
 return (
     <div>
       <h2>This is my Movies Component.</h2>
+
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Add Movie Year: </label>
           <input type="text"
             className="form-control"
             value={year}
-            onChange={(e) => { setTitle(e.target.value) }}
+            onChange={(e) => { setYear(e.target.value) }}
           />
+        </div>
+
+        <div className="form-group">
+            <label>Add Movie Poster: </label>
+            <input type="text"
+                className="form-control"
+                value={poster}
+                onChange={(e) => { setPoster(e.target.value) }}
+            />
         </div>
         <input type="submit" value="Add Movie" />
       </form>
+      {props.myMovies && props.myMovies.map(
+        (movie) => {
+          return <MovieItem mymovie={movie} key={movie.imdbID} />
+        }
+      )}
     </div>
   );
 
